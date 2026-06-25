5e30938fa48c36ebb665dd7246b7f9c1438d963f
yeong246809 <yeong246809@gmail.com>
2026-06-25 17:40:15 +0900

    add all

---
+++ team1-1/src/main/java/org/example/team11/Team11Application.java
@@ -0,0 +1,13 @@
+package org.example.team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team11Application.class, args);
+    }
+
+}
