b32db22f8b4d245c1556918cc7bc629b5ee1d5e8
yeong246809 <yeong246809@gmail.com>
2026-07-03 14:19:55 +0900

    commit first

---
+++ K_market/src/main/java/org/example/k_market/KMarketApplication.java
@@ -0,0 +1,13 @@
+package org.example.k_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KMarketApplication.class, args);
+    }
+
+}
