5bb22374fe5f8160655b971de8c4eeae8170590d
20153254kce <kceuni0405@gmail.com>
2026-07-06 12:59:34 +0900

    0.0.1

--- src/main/java/org/example/k_market/repository/CuponDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.k_market.repository;
-
-import org.example.k_market.entity.CuponDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CuponDetailsRepository extends JpaRepository<CuponDetails, Long> {
-}
--- src/main/java/org/example/k_market/repository/CuponRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.k_market.repository;
-
-import org.example.k_market.entity.Cupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CuponRepository extends JpaRepository<Cupon, Long> {
-}
